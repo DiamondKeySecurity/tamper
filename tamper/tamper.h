c318fab9bf09f258af030e5b1d78424be4b498ee
Diamond Key Security™ <38894928+DiamondKeySecurity@users.noreply.github.com>
2019-10-09 14:45:15 -0500

    Updated to BSD 3 license

--- tamper/tamper.h
+++ tamper/tamper.h
@@ -3,17 +3,8 @@
  * ---------------
  * tamper initialization and execution loop.
  *
- * Copyright © 2018, 2019 Diamond Key Security, NFP
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; version 2
- * of the License only.
- * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, If not, see <https://www.gnu.org/licenses/>.*/
+ * Copyright Â© 2018, 2019 Diamond Key Security, NFP
+*/
 
 /* Copyright (c) 2016, NORDUnet A/S. */
 /*
